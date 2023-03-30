5ec9f983b950b76ddcb9d42a44a17061153c0c06
Mitar Djakovic <mitar-djakovic2401993@hotmail.com>
2023-03-30 14:04:52 +0200

    Add basic project setup

---
+++ src/server.ts
@@ -0,0 +1,15 @@
+import bodyParser from 'body-parser';
+import cors from 'cors';
+import express from 'express';
+
+const app = express();
+
+app.use(cors());
+app.use(bodyParser.json());
+// app.use(api);
+
+const PORT = 8000;
+
+app.listen(PORT, async () => {
+	console.log(`Server is listening on port ${PORT}`);
+});
